ea45e0caf3d8f3166882b0bf1fa9ec1cf2cf5ddc
yahuan.jin <yahuan.jin@outlook.com>
2020-04-18 23:34:05 +0800

    feat[api]: 完善请求参数

--- courier-api/src/main/java/cn/jinyahuan/commons/courier/api/model/AbstractRequest.java
+++ courier-api/src/main/java/cn/jinyahuan/commons/courier/api/model/AbstractRequest.java
@@ -16,10 +16,53 @@
 
 package cn.jinyahuan.commons.courier.api.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
+ * todo 抽象的请求信息类。
+ *
  * @author Yahuan Jin
  * @since 0.1
  */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public abstract class AbstractRequest implements RequestAttribute {
+    /** 手机号 */
+    protected String phone;
+    /** 模板编码 */
+    protected String templateCode;
+    /** 参数列表 */
+    protected String[] params;
+    /** 拓展参数列表（可能会去除的参数） */
+    @Deprecated
+    private Map<String, String> extendedParams;
 
+    public AbstractRequest() {
+        init();
+    }
+
+    /**
+     * 添加额外的参数。
+     *
+     * @param name
+     * @param value
+     */
+    protected void addExtendedParam(String name, String value) {
+        extendedParams.put(name, value);
+    }
+
+    /**
+     * 初始化。
+     */
+    protected void init() {
+        extendedParams = new LinkedHashMap<>();
+    }
 }
